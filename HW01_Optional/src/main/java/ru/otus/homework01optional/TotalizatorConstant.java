e4f7da62e955fe7305617b1206cc043342551e9b
unknown <D:\Programs\The Bat!\Mail>
2018-01-12 22:34:50 +0400

    optional homework 01 added

---
+++ HW01_Optional/src/main/java/ru/otus/homework01optional/TotalizatorConstant.java
@@ -0,0 +1,8 @@
+package ru.otus.homework01optional;
+
+public class TotalizatorConstant {
+
+    public static final String TOTALIZATOR_URL = "/result";
+    public static final String WINNER_REQUEST_ATTRIBUTE = "winnerAttribute";
+}
+
